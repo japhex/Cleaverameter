3c89bbaed6b2c9c726488eacdd519be614cf7447
Jamie Lawrence <jlawrence@pcam00002.local>
2017-10-29 18:33:29 +0000

    Changing client schema

--- models/client.js
+++ models/client.js
@@ -4,7 +4,12 @@ module.exports = (sequelize, DataTypes) => {
     const Client = sequelize.define('client', {
         name: {type: DataTypes.STRING},
         version: {type: DataTypes.STRING},
-        schema: {type: DataTypes.STRING}
+        schema: {type: DataTypes.STRING},
+        current_release: {type: DataTypes.STRING},
+        fcp_label: {type: DataTypes.STRING},
+        last_successful_build: {type: DataTypes.DATE},
+        last_failed_build: {type: DataTypes.DATE},
+        build_status: {type: DataTypes.STRING}
     }, {
         timestamps: false,
         freezeTableName: true,
