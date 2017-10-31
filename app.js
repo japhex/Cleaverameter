cddd7cd631682ace24a9fd20dab2feec166ae525
Jamie Lawrence <jlawrence@pcam00002.btexpedite.co.uk>
2017-10-31 15:38:16 +0000

    System parameter groups API

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const routes = require('./routes/index');
+const clientModels = require('./routes/client');
 
 const app = express();
 
@@ -30,7 +31,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({secret: 'ssshhhhh'}));
+
+// Bind routes
 app.use('/', routes);
+app.use('/client', clientModels);
 
 /// catch 404 and forward to error handler
 app.use((req, res, next) => {
--- routes/index.js
+++ routes/index.js
@@ -19,34 +19,4 @@ router.get('/', (req, res) => {
     });
 });
 
-/* GET client parameters. */
-router.get('/client/parameters', (req, res) => {
-    if (req.session.clientId === undefined) {
-        res.redirect('/');
-    } else {
-        models.system_parameter.findAll({where:{client_context:req.session.clientId}, include: [{ all: true, nested: true }], limit:100}).then(parameters => {
-            res.render('models/system_parameter', {
-                title: 'Cleaverameter | Parameters',
-                parameters: parameters
-            });
-        });
-    }
-});
-
-/* POST client context. */
-router.post('/client/set-context', (req, res) => {
-    let clientId = req.body.clientId;
-
-    req.session.clientId = clientId;
-
-    models.client.findOne({where:{id:clientId}}).then(client => {
-        req.session.clientName = client.name;
-        req.session.clientVersion = client.version;
-        req.session.currentRelease = client.current_release;
-        req.session.fcpLabel = client.fcp_label;
-        req.session.buildStatus = client.build_status;
-        res.redirect('/client/parameters');
-    });
-});
-
 module.exports = router;
