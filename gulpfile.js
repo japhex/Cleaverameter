b6fefc082a2b112a99f4d1bcef78348def071d7e
Jamie Lawrence <jlawrence@pcam00002.btexpedite.co.uk>
2017-10-31 16:41:59 +0000

    Splitting out headers

--- gulpfile.js
+++ gulpfile.js
@@ -32,7 +32,7 @@ gulp.task('js', () => {
 });
 
 gulp.task('watch', () => {
-    gulp.watch('./src/scss/*.scss', ['sass']);
+    gulp.watch('./src/scss/**/*.scss', ['sass']);
     gulp.watch('./src/js/*.js', ['js']);
 });
 
