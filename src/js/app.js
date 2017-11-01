d54aca6964a660638a5d28c4cd04e83b65be70ae
Jamie Lawrence <jlawrence@pcam00002.btexpedite.co.uk>
2017-11-01 11:08:01 +0000

    Added business rule tooltips

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,9 @@
 import * as stickyHeader from './modules/sticky-header';
 import * as secureAdmin from './modules/secure-admin';
+import * as tooltips from './utils/tooltips';
 
 stickyHeader.stickyHeader();
+tooltips.tooltips();
 secureAdmin.onSecureFunction(function () {
     document.write('<img src="/img/secure/secure.jpg" />');
 });
